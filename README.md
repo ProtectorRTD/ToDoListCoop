d802dc64a32a8a5b7d7e52f4e1e625870a8ef57a
Yehor Oliinyk <72699799+ProtectorRTD@users.noreply.github.com>
2022-07-07 11:37:04 +0200

    Check github

---
+++ README.md
@@ -0,0 +1,9 @@
+## Project with DemyDeni
+
+
+## Folder Structure (In future)
+
+The workspace contains two folders by default, where:
+
+- `lib`: the folder to maintain dependencies
+- `src:`: The folder with implementation 
---
+++ src/main/java/com/example/controllers/CheckContoller.java
@@ -0,0 +1,17 @@
+package com.example.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CheckContoller {
+
+    //localhost:8080/demo/start - Адрес, контроллер сможет принять запрос гет
+    @GetMapping(value = "/start")
+    public String doSmth(Model model)
+    {
+        System.out.println("Proverka");
+        return "d";
+    }
+}
